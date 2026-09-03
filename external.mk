4ce06a95c36e35222782ab3d3e0157c90e466f52
dufin <blackrat.academy@gmail.com>
2026-09-03 18:34:39 -0300

    Integrate SV6256P driver with Buildroot

---
+++ external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_RK322X_SERVER_PATH)/package/*/*.mk))
---
+++ package/ssv6x5x/ssv6x5x.mk
@@ -0,0 +1,17 @@
+SSV6X5X_VERSION = local
+SSV6X5X_SITE = $(BR2_EXTERNAL_RK322X_SERVER_PATH)/package/ssv6x5x/src
+SSV6X5X_SITE_METHOD = local
+
+SSV6X5X_MODULE_SUBDIRS = .
+
+define SSV6X5X_INSTALL_FIRMWARE
+	$(INSTALL) -D -m 0644 $(@D)/ssv6x5x-sw.bin \
+		$(TARGET_DIR)/lib/firmware/ssv6x5x-sw.bin
+	$(INSTALL) -D -m 0644 $(@D)/ssv6x5x-wifi.cfg \
+		$(TARGET_DIR)/lib/firmware/ssv6x5x-wifi.cfg
+endef
+
+SSV6X5X_POST_INSTALL_TARGET_HOOKS += SSV6X5X_INSTALL_FIRMWARE
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
